129bfaa2178229b1d7c9ce4d4fa2599bfb50a765
cppcodes <105802485+cppcodes@users.noreply.github.com>
2022-05-20 08:41:16 -0400

    Create find_element_in_vector_of_integers.cpp
    This source code finds an element index in a vector of integers using C++.

---
+++ find_element_in_vector_of_integers.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int64_t find_element_in_vector_of_integers(int element, vector <int>& vector_of_integers)
+{
+	// Define an iterator pointing to the element in vector of integers
+	vector<int>::iterator it = find(vector_of_integers.begin(), vector_of_integers.end(), element); // O(N)
+
+	// Check if element was found in vector of integers
+	if (it == vector_of_integers.end())
+	{
+		// Element not found
+		return -1;
+	}
+
+	// Get element index inside vector of integers
+	int64_t index{ distance(vector_of_integers.begin(), it) }; // O(1)
+
+	return index;
+}
+
+int main()
+{
+	vector<int> vector_of_integers{ 7, 10, 75, 16, 4, 2, 29, 5, 73 };
+	int element{ 16 };
+	int64_t index = find_element_in_vector_of_integers(element, vector_of_integers);
+
+	cout << "The index of '" << element << "' is " << index << endl;
+
+	return 0;
+}
