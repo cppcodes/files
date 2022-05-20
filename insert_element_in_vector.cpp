7c03dbd546107a21386c2c9067c94865cbdb6202
cppcodes <105802485+cppcodes@users.noreply.github.com>
2022-05-20 11:12:20 -0400

    Update insert_element_in_vector.cpp
    Added a for loop to add elements at the end (8,9,10) of the vector of integers.

--- insert_element_in_vector.cpp
+++ insert_element_in_vector.cpp
@@ -12,8 +12,10 @@ int main()
 	vector_of_integers.push_back(5); // O(1)
 	vector_of_integers.push_back(7); // O(1)
 
-	// Printing out all the elements of vector of integers - Method 1
-	copy(vector_of_integers.begin(), vector_of_integers.end(), ostream_iterator<int>(cout, " ")); // 1 3 5 7
+	for (int i = 8; i <= 10; i++) 
+		vector_of_integers.push_back(i);
+										 // Printing out all the elements of vector of integers - Method 1
+	copy(vector_of_integers.begin(), vector_of_integers.end(), ostream_iterator<int>(cout, " ")); // 1 3 5 7 8 9 10
 	cout << endl << endl;
 
 	// Inserting '2' at index 1
@@ -23,24 +25,24 @@ int main()
 
 	// Printing out all the elements of vector of integers - Method 2
 	for (auto const& element : vector_of_integers)
-		std::cout << element << " "; // 1 2 3 5 7
+		std::cout << element << " "; // 1 2 3 5 7 8 9 10
 	cout << endl << endl;
 
 	// "it" no longer valid, get a new one:
 	it = vector_of_integers.begin();
 	vector_of_integers.insert(it + 4, 6); // O(N+M) => M is size of elements to be inserted
-	
+
 	// Printing out all the elements of vector of integers - Method 3
 	for (it = vector_of_integers.begin(); it != vector_of_integers.end(); it++)
-		std::cout << *it << ' '; // 1 2 3 5 6 7
+		std::cout << *it << ' '; // 1 2 3 5 6 7 8 9 10
 	cout << endl << endl;
 
 	vector<int> new_vector_to_be_inserted(7, 4);
 	vector_of_integers.insert(vector_of_integers.begin() + 3, new_vector_to_be_inserted.begin(), new_vector_to_be_inserted.end()); // O(N+M) => M is size of elements to be inserted
-	
+
 	// Printing out all the elements of vector of integers - Method 4
 	for (int i = 0; i < vector_of_integers.size(); i++)
-		cout << vector_of_integers.at(i) << ' '; // 1 2 3 4 4 4 4 4 4 4 5 6 7
+		cout << vector_of_integers.at(i) << ' '; // 1 2 3 4 4 4 4 4 4 4 5 6 7 8 9 10
 	cout << endl << endl;
 
 	return 0;
