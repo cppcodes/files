58b11fc955dd8780a2f008ca5f7dd8e6b27e5bbe
cppcodes <105802485+cppcodes@users.noreply.github.com>
2022-05-20 11:16:26 -0400

    Update insert_element_in_vector.cpp
    Improved source code formatting.

--- insert_element_in_vector.cpp
+++ insert_element_in_vector.cpp
@@ -12,9 +12,10 @@ int main()
 	vector_of_integers.push_back(5); // O(1)
 	vector_of_integers.push_back(7); // O(1)
 
-	for (int i = 8; i <= 10; i++) 
+	for (int i = 8; i <= 10; i++)
 		vector_of_integers.push_back(i);
-										 // Printing out all the elements of vector of integers - Method 1
+
+	// Printing out all the elements of vector of integers - Method 1
 	copy(vector_of_integers.begin(), vector_of_integers.end(), ostream_iterator<int>(cout, " ")); // 1 3 5 7 8 9 10
 	cout << endl << endl;
 
@@ -28,7 +29,7 @@ int main()
 		std::cout << element << " "; // 1 2 3 5 7 8 9 10
 	cout << endl << endl;
 
-	// "it" no longer valid, get a new one:
+	// "it" no longer valid, get a new one
 	it = vector_of_integers.begin();
 	vector_of_integers.insert(it + 4, 6); // O(N+M) => M is size of elements to be inserted
 
@@ -37,6 +38,7 @@ int main()
 		std::cout << *it << ' '; // 1 2 3 5 6 7 8 9 10
 	cout << endl << endl;
 
+	// insert '4' 7 times at index 3
 	vector<int> new_vector_to_be_inserted(7, 4);
 	vector_of_integers.insert(vector_of_integers.begin() + 3, new_vector_to_be_inserted.begin(), new_vector_to_be_inserted.end()); // O(N+M) => M is size of elements to be inserted
 
