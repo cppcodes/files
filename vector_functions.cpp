405cc4f2d898dd289246e4731370a741601b1291
cppcodes <105802485+cppcodes@users.noreply.github.com>
2022-05-25 07:23:03 -0400

    Update vector_functions.cpp
    Renamed function print_list to print_vector

--- vector_functions.cpp
+++ vector_functions.cpp
@@ -4,7 +4,7 @@
 
 using namespace std;
 
-void print_list(const vector <int>& vector_of_integers)
+void print_vector(const vector <int>& vector_of_integers)
 {
 	copy(vector_of_integers.begin(), vector_of_integers.end(), ostream_iterator<int>(cout, " "));
 	cout << endl;
@@ -14,7 +14,7 @@ void print_list(const vector <int>& vector_of_integers)
 int main()
 {
 	vector<int> vector_of_integers{ 1,2,3,4,5,6,7 };
-	print_list(vector_of_integers); // 1 2 3 4 5 6 7
+	print_vector(vector_of_integers); // 1 2 3 4 5 6 7
 
 	cout << vector_of_integers.front() << endl; // 1 // O(1)
 	cout << vector_of_integers.back() << endl; // 7 // O(1)
@@ -25,29 +25,29 @@ int main()
 	vector<int> new_vector_of_integers{ 7,16 };
 	new_vector_of_integers[0] = -1; // O(1)
 	new_vector_of_integers[1] = -3; // O(1)
-	print_list(new_vector_of_integers); // -1 -3
+	print_vector(new_vector_of_integers); // -1 -3
 
 	new_vector_of_integers = vector_of_integers; // O(N)
-	print_list(new_vector_of_integers); // 1 2 3 4 5 6 7
+	print_vector(new_vector_of_integers); // 1 2 3 4 5 6 7
 
 	cout << vector_of_integers.empty() << endl; // 0 // O(1)
 	vector_of_integers.clear(); // O(1) if elements need NOT be destroyed (trivially-destructible types, such as scalar or PODs) otherwise, O(N)
 	cout << vector_of_integers.empty() << endl; // 1 // O(1)
-	print_list(new_vector_of_integers); // 1 2 3 4 5 6 7
+	print_vector(new_vector_of_integers); // 1 2 3 4 5 6 7
 
 
 	vector_of_integers.push_back(5); // O(1)
-	print_list(vector_of_integers); // 5
+	print_vector(vector_of_integers); // 5
 
 	vector_of_integers.insert(vector_of_integers.begin(), 1); // O(N+M) => M is size of elements to be inserted
-	print_list(vector_of_integers); // 1 5
+	print_vector(vector_of_integers); // 1 5
 
 	vector_of_integers.insert(vector_of_integers.begin() + 1, 3); // O(N+M) => M is size of elements to be inserted
-	print_list(vector_of_integers); // 1 3 5
+	print_vector(vector_of_integers); // 1 3 5
 
 	array<int, 3> new_array{ 7,9,11 };
 	vector_of_integers.insert(vector_of_integers.end(), new_array.begin(), new_array.end()); // O(N+M) => M is size of elements to be inserted
-	print_list(vector_of_integers); // 1 3 5 7 9 11
+	print_vector(vector_of_integers); // 1 3 5 7 9 11
 
 	int* p = vector_of_integers.data();
 	*p = -1;
@@ -58,20 +58,20 @@ int main()
 	p[3] = -9;
 	++p;
 	p[3] = -11;
-	print_list(vector_of_integers); // -1 -3 -5 -7 -9 -11
+	print_vector(vector_of_integers); // -1 -3 -5 -7 -9 -11
 
 	vector_of_integers.erase(vector_of_integers.begin() + 4); // O(N+M) => M is the number of elements erased (destructions)
-	print_list(vector_of_integers); // -1 -3 -5 -7 -11
+	print_vector(vector_of_integers); // -1 -3 -5 -7 -11
 
 	vector_of_integers.erase(vector_of_integers.begin() + 1, vector_of_integers.end() - 1);  // O(N+M) => M is the number of elements erased (destructions)
-	print_list(vector_of_integers); // -1 -11
+	print_vector(vector_of_integers); // -1 -11
 
 	vector_of_integers.swap(new_vector_of_integers); // O(1)
-	print_list(vector_of_integers); // 1 2 3 4 5 6 7
-	print_list(new_vector_of_integers); // -1
+	print_vector(vector_of_integers); // 1 2 3 4 5 6 7
+	print_vector(new_vector_of_integers); // -1
 
 	vector_of_integers.pop_back();  // O(1)
-	print_list(vector_of_integers); // 1 2 3 4 5 6
+	print_vector(vector_of_integers); // 1 2 3 4 5 6
 
 	cout << "size: " << vector_of_integers.size() << '\n'; // 7 // O(1) // Returns the number of elements in the vector.
 	cout << "capacity: " << vector_of_integers.capacity() << '\n'; // 7 // O(1) // Returns the size of the storage space CURRENTLY allocated for the vector, expressed in terms of elements.
